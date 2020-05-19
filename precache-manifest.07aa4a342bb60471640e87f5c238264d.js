aec7eea081eeb797ba3843ab759ed935c4acac02
ninadthepade <ninad.thepade@gmail.com>
2020-05-19 21:10:44 +0530

    Updates

---
+++ precache-manifest.07aa4a342bb60471640e87f5c238264d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dec8a25f6b1ca61454005a0975e530f",
+    "url": "/color-game/index.html"
+  },
+  {
+    "revision": "7d074a47d280c2342136",
+    "url": "/color-game/static/css/main.af20f21b.chunk.css"
+  },
+  {
+    "revision": "ede91a7c0b78030ee841",
+    "url": "/color-game/static/js/2.db874750.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-game/static/js/2.db874750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d074a47d280c2342136",
+    "url": "/color-game/static/js/main.5d6bc57d.chunk.js"
+  },
+  {
+    "revision": "b822b180503cd7189b1a",
+    "url": "/color-game/static/js/runtime-main.26627553.js"
+  }
+]);
