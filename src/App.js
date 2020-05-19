52b4641d39d73f4eb30e44a2708f3ca6244ff1eb
ninadthepade <ninad.thepade@gmail.com>
2020-05-19 19:36:32 +0530

    Adding color grid

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import GameControls from './components/GameControls/GameControls'
 import ColorPicker from './components/ColorPicker/ColorPicker'
 import GridBoard from './components/GridBoard/GridBoard'
+import { Graph } from './components/Graph/Graph';
 
 import './App.css';
 
@@ -11,24 +12,28 @@ const COLORS = ['red', 'blue', 'green', 'yellow', 'coral']
 
 function App() {
   const [movesCounter, setMovesCounter] = useState(0)
+  const [graph, setGraph] = useState(new Graph(SIZE))
 
   const incrementCount = () => {
     setMovesCounter(prevState => prevState + 1)
   }
   
   const startClicked = () => {
+    setGraph(new Graph(SIZE))
     setMovesCounter(0)
   }
 
   const colorFill = (color) => {
-    console.log(color + ' color clicked')
+    let colorIndex = COLORS.indexOf(color)
+    graph.colorFill(colorIndex);
   }
 
+  console.log('Grid:', graph)
   return (
     <div className="App">
       <GameControls movesCounter={movesCounter} startClicked={startClicked}></GameControls>
       <ColorPicker colors={COLORS} colorFill={colorFill} incrementCount={incrementCount}></ColorPicker>
-      <GridBoard colors={COLORS} size={SIZE}></GridBoard>
+      <GridBoard grid={graph} colors={COLORS} size={SIZE}></GridBoard>
     </div>
   );
 }
--- src/components/GridBoard/GridBoard.js
+++ src/components/GridBoard/GridBoard.js
@@ -2,10 +2,22 @@ import React from 'react'
 
 import './GridBoard.css'
 
-const GridBoard = (colors) => {
+const GridBoard = ({grid, colors}) => {
+  let rows = <div> No grid data</div>
+  
+  if(grid.nodes) {
+    rows = Object.keys(grid.nodes).map((v) => {
+      let color = colors[grid.nodes[v].color]
+      let size = grid.size
+      let classString = "cell-" + size + " " + color;
+  
+      return <div key={v} className={classString}></div>
+    });
+  }
+
   return (
     <div className="grid-board">
-      Grid Board component
+      {rows}
     </div>
   )
 }
